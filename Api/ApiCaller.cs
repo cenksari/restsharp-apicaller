cf841ae06b2e31f616b7a2319e1f0dd6a1ed15c4
cenksari <cenk@cenksari.com>
2018-02-22 14:35:28 +0300

    Api model added.

--- Api/ApiCaller.cs
+++ Api/ApiCaller.cs
@@ -2,7 +2,6 @@
 {
 	using RestSharp;
 	using System;
-	using System.Collections.Generic;
 	using System.Threading;
 	using System.Threading.Tasks;
 
@@ -66,26 +65,5 @@
 
 			return returnedValue;
 		}
-	}
-
-	public class RestResponse
-	{
-		public bool Status { get; set; }
-		public string Message { get; set; }
-		public string Response { get; set; }
-		public string ErrorCode { get; set; }
-	}
-
-	public class RestRequest
-	{
-		public string Method { get; set; }
-		public string Endpoint { get; set; }
-		public List<RestParameter> Parameters { get; set; }
-	}
-
-	public class RestParameter
-	{
-		public string Name { get; set; }
-		public string Value { get; set; }
 	}
 }
---
+++ Api/ApiModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Api
+{
+	using System.Collections.Generic;
+
+	public class RestResponse
+	{
+		public bool Status { get; set; }
+		public string Message { get; set; }
+		public string Response { get; set; }
+		public string ErrorCode { get; set; }
+	}
+
+	public class RestRequest
+	{
+		public string Method { get; set; }
+		public string Endpoint { get; set; }
+		public List<RestParameter> Parameters { get; set; }
+	}
+
+	public class RestParameter
+	{
+		public string Name { get; set; }
+		public string Value { get; set; }
+	}
+}
